f81c9fbc8b5f5d70394a2f4333aa9d353b10e6a8
GreenpixDev <kol-roman.green@mail.ru>
2022-04-25 22:08:10 +0700

    Мини фикс

--- app/src/main/java/ru/hits/android/axolot/blueprint/node/NodeInvokable.kt
+++ app/src/main/java/ru/hits/android/axolot/blueprint/node/NodeInvokable.kt
@@ -2,7 +2,7 @@ package ru.hits.android.axolot.blueprint.node
 
 import ru.hits.android.axolot.blueprint.context.Context
 
-interface NodeInvokable<T> {
+interface NodeInvokable<T> : Node {
 
     operator fun invoke(context: Context): T
 
