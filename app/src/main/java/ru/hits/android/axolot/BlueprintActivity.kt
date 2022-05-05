7cef3d29031c0b95e8902bc0ee373321d0ef8a8d
Wafer_ST <zziikk2003@gmail.com>
2022-05-05 19:37:45 +0700

    можно перемещать квадрат на экране BlueprintActivity.kt P.S: С перестановкой самих кастомных блоков проблема (не только у меня), но Андрей решает эту проблему, поэтому ждем

--- app/src/main/java/ru/hits/android/axolot/BlueprintActivity.kt
+++ app/src/main/java/ru/hits/android/axolot/BlueprintActivity.kt
@@ -1,22 +1,135 @@
 package ru.hits.android.axolot
 
+import android.content.ClipData
+import android.content.ClipDescription
+import android.view.View
+import android.os.Build
 import android.os.Bundle
+import android.content.Intent
+import android.view.DragEvent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import ru.hits.android.axolot.databinding.ActivityBlueprintBinding
 import ru.hits.android.axolot.view.BlockView
 
 class BlueprintActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBlueprintBinding
+    var menuIsVisible = true
+    private var blocksList = mutableListOf<BlockView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBlueprintBinding.inflate(layoutInflater)
         setContentView(binding.blueprintLayout)
 
-        binding.addBlock.setOnClickListener() {
-            binding.blueprintLayout.addView(BlockView(this, null))
+        //скрывание и показ меню
+        binding.showMenu.setOnClickListener() {
+            if (menuIsVisible) {
+                binding.menu.visibility = View.GONE
+                menuIsVisible = false
+            } else {
+                binding.menu.visibility = View.VISIBLE
+                menuIsVisible = true
+            }
+        }
+
+        //переключение на страницу с настройками
+        binding.ToPageSettings.setOnClickListener() {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+
+        //добавление блока
+        binding.addVar.setOnClickListener() {
+            binding.codeField.addView(BlockView(this))
+            blocksList.add(BlockView(this))
+        }
+
+        //перещение блоков добавленных первоначально в xml
+        attachViewDragListener()
+        binding.blueprintLayout.setOnDragListener(blockDragListener)
+    }
+
+    private fun attachViewDragListener() {
+        binding.varBlock.setOnLongClickListener {view: View ->
+
+            val clipText = "This is out Clipdata text"
+            val item = ClipData.Item(clipText)
+
+            val dataToDrag = ClipData (clipText, arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN), item)
+
+            val blockShadow = View.DragShadowBuilder(view)
+
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                //support pre-Nougat versions
+                @Suppress("DEPRECATION")
+                view.startDragAndDrop(dataToDrag, blockShadow, view, 0)
+            } else {
+                //supports Nougat and beyond
+                view.startDragAndDrop(dataToDrag, blockShadow, view, 0)
+            }
+
+            view.visibility = View.INVISIBLE
+
+            true
         }
     }
 
+    private val blockDragListener = View.OnDragListener { view, dragEvent->
+        val draggableItem = dragEvent.localState as View
+
+        when (dragEvent.action) {
+            DragEvent.ACTION_DRAG_STARTED -> {
+                true
+            }
+
+            DragEvent.ACTION_DRAG_ENTERED -> {
+                binding.varBlock.alpha = 0.3f
+                true
+            }
+
+            DragEvent.ACTION_DRAG_LOCATION -> {
+                true
+            }
+
+            DragEvent.ACTION_DRAG_EXITED -> {
+                binding.varBlock.alpha = 1.0f
+                draggableItem.visibility = View.VISIBLE
+                view.invalidate()
+                true
+            }
+
+            DragEvent.ACTION_DROP -> {
+                binding.varBlock.alpha = 1.0f
+
+                draggableItem.x = dragEvent.x - (draggableItem.width/2)
+                draggableItem.y = dragEvent.y - (draggableItem.height/2)
+
+                val parent = draggableItem.parent as ConstraintLayout
+
+                parent.removeView(draggableItem)
+
+                val dropArea = view as ConstraintLayout
+
+                dropArea.addView(draggableItem)
+
+                if (dragEvent.clipDescription.hasMimeType((ClipDescription.MIMETYPE_TEXT_PLAIN))) {
+                    val draggedData = dragEvent.clipData.getItemAt(0).text
+                }
+
+                true
+            }
+
+            DragEvent.ACTION_DRAG_ENDED -> {
+                draggableItem.visibility = View.VISIBLE
+
+                view.invalidate()
+                true
+            }
+
+            else -> {
+                false
+            }
+        }
+    }
 }
--- app/src/main/java/ru/hits/android/axolot/view/BlockView.kt
+++ app/src/main/java/ru/hits/android/axolot/view/BlockView.kt
@@ -1,12 +1,27 @@
 package ru.hits.android.axolot.view
 
 import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipDescription
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Build
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
+import android.view.DragEvent
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.activity_blueprint.view.*
 import ru.hits.android.axolot.R
-import ru.hits.android.axolot.databinding.BlockType1Binding
+import ru.hits.android.axolot.databinding.ActivityBlueprintBinding
+import ru.hits.android.axolot.databinding.BlockItemBinding
 
 
 @SuppressLint("ViewConstructor")
@@ -17,14 +32,85 @@ class BlockView @JvmOverloads constructor(
     defstyleRes: Int = 0
 ): ConstraintLayout(context, attrs, defstyleAttr, defstyleRes) {
 
-    private val binding: BlockType1Binding
+    private val binding: BlockItemBinding
+    private lateinit var bin: ActivityBlueprintBinding
 
     init {
         val inflater = LayoutInflater.from(context)
-        inflater.inflate(R.layout.block_type1, this, true)
-        binding = BlockType1Binding.bind(this)
+        inflater.inflate(R.layout.block_item, this, true)
+        binding = BlockItemBinding.bind(this)
 
+        //создание атрибутов
         initializeAttributes(attrs, defstyleAttr, defstyleRes)
+
+        //перемещение
+        changeMove()
+    }
+
+    private fun changeMove() {
+        //переменная для drag and drop
+        val blockDragListener = OnDragListener { view, dragEvent->
+            val draggableItem = dragEvent.localState as View
+
+            when (dragEvent.action) {
+                DragEvent.ACTION_DRAG_STARTED -> {
+                    true
+                }
+
+                DragEvent.ACTION_DRAG_ENTERED -> {
+                    binding.block.alpha = 0.3f
+                    true
+                }
+
+                DragEvent.ACTION_DRAG_LOCATION -> {
+                    true
+                }
+
+                DragEvent.ACTION_DRAG_EXITED -> {
+                    binding.block.alpha = 1.0f
+                    //draggableItem.visibility = View.VISIBLE
+
+                    view.invalidate()
+                    true
+                }
+
+                DragEvent.ACTION_DROP -> {
+                    binding.block.alpha = 1.0f
+
+                    draggableItem.x = dragEvent.x - (draggableItem.width/2)
+                    draggableItem.y = dragEvent.y - (draggableItem.height/2)
+
+                    val parent = draggableItem.parent as ConstraintLayout
+
+                    parent.removeView(draggableItem)
+
+                    val dropArea = view as ConstraintLayout
+
+                    dropArea.addView(draggableItem)
+
+                    if (dragEvent.clipDescription.hasMimeType((ClipDescription.MIMETYPE_TEXT_PLAIN))) {
+                        val draggedData = dragEvent.clipData.getItemAt(0).text
+                    }
+
+                    true
+                }
+
+                DragEvent.ACTION_DRAG_ENDED -> {
+                    draggableItem.visibility = View.VISIBLE
+
+                    view.invalidate()
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
+
+        attachViewDragListener()
+        bin = ActivityBlueprintBinding.inflate(LayoutInflater.from(context))
+        binding.block.setOnDragListener(blockDragListener)
     }
 
     @SuppressLint("Recycle")
@@ -38,30 +124,47 @@ class BlockView @JvmOverloads constructor(
             val blockTitle = typedArray.getString(R.styleable.BlockView_blockTitle)
             setTitleBlockText(blockTitle)
 
-//            val blockPropertyText = typedArray.getString(R.styleable.BlockView_blockPropertyText)
-//            binding.property.text = blockPropertyText
-//
-//            val blockConnection = typedArray.getBoolean(R.styleable.BlockView_btnConnection, false)
-//            if (blockConnection) {
-//                binding.node.backgroundTintList = ColorStateList.createFromXml(resources, XmlPullParser.FEATURE_PROCESS_NAMESPACES, false)
-//            } else {
-//                binding.node.backgroundTintList = ColorStateList.createFromXml("@drawable/button_shape_circle2")
-//            }
+            val bgBlock = typedArray.getColor(R.styleable.BlockView_bgBlock, Color.WHITE)
+            block.backgroundTintList = ColorStateList.valueOf(bgBlock)
 
         }
 
         typedArray.recycle()
     }
 
-//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-//
-//    }
-
-    fun setTitleBlockText(title:String?) {
+    //присваивание названия
+    private fun setTitleBlockText(title:String?) {
         binding.title.text = title ?: "block"
     }
 
-    fun determineConnection() {
 
+    private fun attachViewDragListener(){
+        binding.block.setOnLongClickListener {view: View ->
+
+            val clipText = "This is out ClipData text"
+            val item = ClipData.Item(clipText)
+
+            val dataToDrag = ClipData (clipText, arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN), item)
+
+            val blockShadow = View.DragShadowBuilder(view)
+
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                //support pre-Nougat versions
+                @Suppress("DEPRECATION")
+                view.startDragAndDrop(dataToDrag, blockShadow, view, 0)
+            } else {
+                //supports Nougat and beyond
+                view.startDragAndDrop(dataToDrag, blockShadow, view, 0)
+            }
+
+            view.visibility = View.INVISIBLE
+
+            true
+        }
     }
 }
+
+//data class Node (
+//    val name: String,
+//    val neighbour: BlockView
+//)
