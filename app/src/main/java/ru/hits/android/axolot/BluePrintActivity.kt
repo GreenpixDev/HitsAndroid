88f4251ac090e04bc189638cfd3903d0645e5a48
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-05-07 13:05:23 +0700

    Добавил список блоков в меню.

--- app/src/main/java/ru/hits/android/axolot/BluePrintActivity.kt
+++ app/src/main/java/ru/hits/android/axolot/BluePrintActivity.kt
@@ -2,11 +2,17 @@ package ru.hits.android.axolot
 
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.content.res.Resources
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_blue_print.*
+import kotlinx.android.synthetic.main.activity_blue_print.view.*
 import ru.hits.android.axolot.databinding.ActivityBluePrintBinding
 
 class BluePrintActivity : AppCompatActivity() {
@@ -17,6 +23,12 @@ class BluePrintActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bluePrintBinding = ActivityBluePrintBinding.inflate(layoutInflater)
         setContentView(bluePrintBinding.root)
+
+        createTextView()
+
+        bluePrintBinding.scrollViewMenu.linearLayoutFunctionsContainer.newFunction.setOnClickListener(){
+            Toast.makeText(applicationContext, "Пока что это затычка", Toast.LENGTH_SHORT).show()
+        }
     }
 
     fun onClickShowMenu(view: View) {
@@ -29,8 +41,47 @@ class BluePrintActivity : AppCompatActivity() {
         }
     }
 
-    fun onSettings(view: View){
+    fun onSettings(view: View) {
         val intent = Intent(this, SettingsActivity::class.java)
         startActivity(intent)
     }
+
+    fun createTextView() {
+        var textSize = 20f
+        var textColor = Color.BLACK
+        var textAlign = View.TEXT_ALIGNMENT_CENTER
+
+        var variables = resources.getStringArray(R.array.btnBlocksVariables)
+        var cycles = resources.getStringArray(R.array.btnBlocksCycles)
+        var conditions = resources.getStringArray(R.array.btnBlocksConditions)
+
+        for (textViewName in variables){
+            var textView = TextView(this)
+            textView.text = textViewName.toString()
+            textView.setTextColor(textColor)
+            textView.textAlignment = textAlign
+            textView.textSize = textSize
+            bluePrintBinding.scrollViewMenu.linearLayoutMenu.linearLayoutVariablesContainer.addView(textView)
+        }
+
+        for (textViewName in cycles){
+            var textView = TextView(this)
+            textView.text = textViewName.toString()
+            textView.setTextColor(textColor)
+            textView.textAlignment = textAlign
+            textView.textSize = textSize
+            bluePrintBinding.scrollViewMenu.linearLayoutMenu.linearLayoutCyclesContainer.addView(textView)
+        }
+
+        for (textViewName in conditions){
+            var textView = TextView(this)
+            textView.text = textViewName.toString()
+            textView.setTextColor(textColor)
+            textView.textAlignment = textAlign
+            textView.textSize = textSize
+            bluePrintBinding.scrollViewMenu.linearLayoutMenu.linearLayoutConditionsContainer.addView(textView)
+        }
+    }
+
+
 }
