853d3f40054755a99796fc36584124ff22b2f8bc
cyberian <fox4080@gmail.com>
2022-05-17 16:21:29 +0700

    Консоль обьект

---
+++ app/src/main/java/ru/hits/android/axolot/console/Console.kt
@@ -0,0 +1,34 @@
+package ru.hits.android.axolot.console
+
+import java.util.*
+
+class Console {
+
+    val toUser : Queue<String> =  LinkedList<String>()
+    val toApp : Queue<String> =  LinkedList<String>()
+
+    fun getStringToApp():String{
+        if(toApp.isEmpty())
+        {
+            return ""
+        }
+        return toApp.remove()
+    }
+
+    fun getStringToUser():String{
+        if(toUser.isEmpty())
+        {
+            return ""
+        }
+        return toUser.remove()
+    }
+
+    fun sendStringToApp(str:String){
+        toApp.add(str)
+    }
+
+    fun sendStringToUser(str:String){
+        toUser.add(str)
+    }
+
+}
