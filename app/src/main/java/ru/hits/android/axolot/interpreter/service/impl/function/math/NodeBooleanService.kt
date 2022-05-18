d4e023312bf11d29d25215cdb46c5313b9435c42
GreenpixDev <kol-roman.green@mail.ru>
2022-05-18 14:20:37 +0700

    Фикс тестов и бесконечный ряд ифов от Вани

--- app/src/main/java/ru/hits/android/axolot/interpreter/service/impl/function/math/NodeBooleanService.kt
+++ app/src/main/java/ru/hits/android/axolot/interpreter/service/impl/function/math/NodeBooleanService.kt
@@ -11,62 +11,81 @@ import ru.hits.android.axolot.interpreter.variable.Variable
 class NodeBooleanService(private val nodeHandlerService: NodeHandlerService) :
     NodeDependencyService {
     override fun invoke(node: Node, context: InterpreterContext): Variable {
-        if (node is NodeBooleanAnd) {
-            // TODO (переделать)
-        } else if (node is NodeBooleanNand) {
-            val first = nodeHandlerService.invoke(
-                node.dependencies[NodeBooleanNand.FIRST]!!,
-                context
-            )[Type.BOOLEAN]!!
-            val second = nodeHandlerService.invoke(
-                node.dependencies[NodeBooleanNand.SECOND]!!,
-                context
-            )[Type.BOOLEAN]!!
-            return Variable(Type.BOOLEAN, if (first) !second else true)
+        when (node) {
+            is NodeBooleanAnd -> {
+                val first = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanAnd.FIRST]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                val second = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanAnd.SECOND]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                return Variable(Type.BOOLEAN, first && second)
+            }
+            is NodeBooleanNand -> {
+                val first = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanNand.FIRST]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                val second = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanNand.SECOND]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                return Variable(Type.BOOLEAN, if (first) !second else true)
+            }
+            is NodeBooleanNor -> {
+                val first = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanNor.FIRST]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                val second = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanNor.SECOND]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                return Variable(Type.BOOLEAN, !(first || second))
+            }
+            is NodeBooleanNot -> {
+                val input = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanNot.INPUT]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                return Variable(Type.BOOLEAN, !input)
+            }
+            is NodeBooleanOr -> {
+                val first = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanOr.FIRST]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                val second = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanOr.SECOND]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                return Variable(Type.BOOLEAN, first || second)
+            }
+            is NodeBooleanXnor -> {
+                val first = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanXnor.FIRST]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                val second = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanXnor.SECOND]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                return Variable(Type.BOOLEAN, (first == second))
+            }
+            is NodeBooleanXor -> {
+                val first = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanXor.FIRST]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                val second = nodeHandlerService.invoke(
+                    node.dependencies[NodeBooleanXor.SECOND]!!,
+                    context
+                )[Type.BOOLEAN]!!
+                return Variable(Type.BOOLEAN, first.xor(second))
+            }
+            else -> throw createIllegalException(node)
         }
-        else if(node is NodeBooleanNor) {
-            val first = nodeHandlerService.invoke(
-                node.dependencies[NodeBooleanNor.FIRST]!!,
-                context
-            )[Type.BOOLEAN]!!
-            val second = nodeHandlerService.invoke(
-                node.dependencies[NodeBooleanNor.SECOND]!!,
-                context
-            )[Type.BOOLEAN]!!
-            return Variable(Type.BOOLEAN, !(first || second))
-        }
-        else if(node is NodeBooleanNot) {
-            val input = nodeHandlerService.invoke(
-                node.dependencies[NodeBooleanNot.INPUT]!!,
-                context
-            )[Type.BOOLEAN]!!
-            return Variable(Type.BOOLEAN, !input)
-        }
-        else if(node is NodeBooleanOr) {
-            // TODO (переделать)
-        }
-        else if(node is NodeBooleanXnor) {
-            val first = nodeHandlerService.invoke(
-                node.dependencies[NodeBooleanXnor.FIRST]!!,
-                context
-            )[Type.BOOLEAN]!!
-            val second = nodeHandlerService.invoke(
-                node.dependencies[NodeBooleanXnor.SECOND]!!,
-                context
-            )[Type.BOOLEAN]!!
-            return Variable(Type.BOOLEAN, (first == second))
-        }
-        else if(node is NodeBooleanXor) {
-            val first = nodeHandlerService.invoke(
-                node.dependencies[NodeBooleanXor.FIRST]!!,
-                context
-            )[Type.BOOLEAN]!!
-            val second = nodeHandlerService.invoke(
-                node.dependencies[NodeBooleanXor.SECOND]!!,
-                context
-            )[Type.BOOLEAN]!!
-            return Variable(Type.BOOLEAN, first.xor(second))
-        }
-        throw createIllegalException(node)
     }
 }
