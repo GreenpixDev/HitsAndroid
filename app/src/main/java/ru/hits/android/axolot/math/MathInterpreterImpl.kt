c95c3411a669f30c037d4bb677d42f00ce69192d
cyberian <fox4080@gmail.com>
2022-05-18 16:44:12 +0700

    Обьекты

---
+++ app/src/main/java/ru/hits/android/axolot/math/MathInterpreterImpl.kt
@@ -0,0 +1,15 @@
+package ru.hits.android.axolot.math
+
+class MathInterpreterImpl : MathInterpreter {
+
+
+    private fun parse() {
+
+    }
+
+    override fun calcExpression(expression: String, variables: Map<String, Double>): Double {
+
+        return 0.0
+    }
+
+}
