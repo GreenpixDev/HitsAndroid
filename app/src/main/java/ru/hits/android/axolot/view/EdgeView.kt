684712c407b8262afd13bba23617cf86307df9c3
cyberian <fox4080@gmail.com>
2022-05-19 15:45:23 +0700

    Готово

--- app/src/main/java/ru/hits/android/axolot/view/EdgeView.kt
+++ app/src/main/java/ru/hits/android/axolot/view/EdgeView.kt
@@ -3,6 +3,7 @@ package ru.hits.android.axolot.view
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 import ru.hits.android.axolot.R
@@ -34,7 +35,7 @@ class EdgeView @JvmOverloads constructor(
         /**
          * Растояние вспомогательных точек
          */
-        const val BUFFER_POINT_DISTANCE = 75f
+        const val BUFFER_POINT_DISTANCE = 200f
 
         /**
          * Нужно ли рисовать вспомогательные точки
@@ -54,47 +55,15 @@ class EdgeView @JvmOverloads constructor(
     init {
         paintBrush.color = context.getThemeColor(R.attr.colorLine)
         paintBrush.strokeWidth = 5f
+
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
         val circleRadius = context.convertDpToPixel(CIRCLE_RADIUS_DP)
-        val drawPoints = generateDrawPoints()
+        val mPath = Path()
 
-        for (i in 0 until drawPoints.size - 1) {
-            canvas?.drawLine(
-                drawPoints[i].x,
-                drawPoints[i].y,
-                drawPoints[i + 1].x,
-                drawPoints[i + 1].y,
-                paintBrush
-            )
-        }
-
-        for (i in 0 until points.size) {
-            canvas?.drawCircle(points[i].x, points[i].y, circleRadius, paintBrush)
-        }
-
-        canvas?.drawLine(
-            points.first().x,
-            points.first().y,
-            drawPoints.first().x,
-            drawPoints.first().y,
-            paintBrush
-        )
-
-        canvas?.drawLine(
-            points.last().x,
-            points.last().y,
-            drawPoints.last().x,
-            drawPoints.last().y,
-            paintBrush
-        )
-
-    }
-
-    private fun generateDrawPoints(): MutableList<Vec2f> {
         val sourcePoints = points.toMutableList()
 
         if (BUFFER_POINT_DRAW_ENABLED) {
@@ -108,90 +77,32 @@ class EdgeView @JvmOverloads constructor(
                 )
             )
             sourcePoints.add(
-                1, Vec2f(
-                    x = (sourcePoints[1].x + points[0].x)/2,
-                    y = (sourcePoints[1].y + points[0].y)/2
-                )
-            )
-            sourcePoints.add(
                 index = sourcePoints.size - 1,
                 element = Vec2f(
                     x = sourcePoints[sourcePoints.size - 1].x - distance,
                     y = sourcePoints[sourcePoints.size - 1].y
                 )
             )
-            sourcePoints.add(
-                index = sourcePoints.size - 1, Vec2f(
-                    x = (sourcePoints[sourcePoints.size - 2].x + points.last().x)/2,
-                    y = (sourcePoints[sourcePoints.size - 2].y + points.last().y)/2
-                )
-            )
-            sourcePoints.removeFirst()
-            sourcePoints.removeLast()
         }
 
-        val drawPoints = mutableListOf<Vec2f>()
-
-        if (sourcePoints.size < 2) {
-            for (i in 0 until sourcePoints.size) {
-                drawPoints.add(sourcePoints[i])
-            }
-            return drawPoints
+        mPath.reset();
+        mPath.moveTo(points.first().x, points.first().y);
+        mPath.cubicTo(
+            sourcePoints[1].x,
+            sourcePoints[1].y,
+            sourcePoints[2].x,
+            sourcePoints[2].y,
+            sourcePoints[3].x,
+            sourcePoints[3].y
+        )
+        paintBrush.setStyle(Paint.Style.STROKE)
+        canvas?.drawPath(mPath, paintBrush)
+        paintBrush.setStyle(Paint.Style.FILL)
+        for (i in 0 until points.size) {
+            canvas?.drawCircle(points[i].x, points[i].y, circleRadius, paintBrush)
         }
 
-        for (i in 0 until sourcePoints.size - 1) {
-            drawPoints.add(sourcePoints[i])
-            var t = 0f
-            val b = 0f
-            val c = 0f
-            val currentPoint = sourcePoints[i]
-            val nextPoint = sourcePoints[i + 1]
-            var next2Point = sourcePoints[i + 1]
-            var previosPoint = sourcePoints[i]
-            if (i + 2 < sourcePoints.size) {
-                next2Point = sourcePoints[i + 2]
-            }
-            if (i != 0) {
-                previosPoint = sourcePoints[i - 1]
-            }
-            val dix =
-                (1f - t) * (1f + b) * (1f + c) / 2f * (currentPoint.x - previosPoint.x) + (1f - t) * (1f - b) * (1f - c) / 2f * (nextPoint.x - currentPoint.x)
-            val diy =
-                (1f - t) * (1f + b) * (1f + c) / 2f * (currentPoint.y - previosPoint.y) + (1f - t) * (1f - b) * (1f - c) / 2f * (nextPoint.y - currentPoint.y)
-            val dinextx =
-                (1f - t) * (1f + b) * (1f - c) / 2f * (nextPoint.x - currentPoint.x) + (1f - t) * (1f - b) * (1f - c) / 2f * (next2Point.x - nextPoint.x)
-            val dinexty =
-                (1f - t) * (1f + b) * (1f - c) / 2f * (nextPoint.y - currentPoint.y) + (1f - t) * (1f - b) * (1f - c) / 2f * (next2Point.y - nextPoint.y)
+    }//Когда то тут были Kochanek–Bartels spline... F... Теперь лишь пустота...
 
-            val step = 1f / POINT_COUNT
-            t = 0f
-            for (j in 0 until POINT_COUNT) {
-                val x: Float =
-                    dix * h3(t) + currentPoint.x * h1(t) + nextPoint.x * h2(t) + dinextx * h4(t);
-                val y: Float =
-                    diy * h3(t) + currentPoint.y * h1(t) + nextPoint.y * h2(t) + dinexty * h4(t);
-                drawPoints.add(Vec2f(x, y))
-                t += step
-            }
 
-        }
-        drawPoints.add(sourcePoints[sourcePoints.size - 1])
-        return drawPoints
-    }
-
-    private fun h1(t: Float): Float {
-        return 2 * t * t * t - 3 * t * t + 1
-    }
-
-    private fun h2(t: Float): Float {
-        return -2 * t * t * t + 3 * t * t;
-    }
-
-    private fun h3(t: Float): Float {
-        return t * t * t - 2 * t * t + t;
-    }
-
-    private fun h4(t: Float): Float {
-        return t * t * t - t * t;
-    }
 }
