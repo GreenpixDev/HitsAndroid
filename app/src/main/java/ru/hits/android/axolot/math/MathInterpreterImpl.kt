eb0e55f2a88a20c42fee48955cefd91871ae9fc9
cyberian <fox4080@gmail.com>
2022-05-19 10:31:09 +0700

    Теперь и в обратной польской

--- app/src/main/java/ru/hits/android/axolot/math/MathInterpreterImpl.kt
+++ app/src/main/java/ru/hits/android/axolot/math/MathInterpreterImpl.kt
@@ -6,9 +6,34 @@ class MathInterpreterImpl : MathInterpreter {
 
     val doublingsOperators = "<< >>"
 
-    val function = "sin( cos( tg( ctg( arcsin( arccos( arctg( arcctg( ln( log( ( ["
+    val functionWithBrakes =
+        "sin( cos( tg( ctg( arcsin( arccos( arctg( arcctg( ln( log( trunc( floor( ceil( abs( sqrt( exp( ( ["
+
+    val mathWithBrakes =
+        "sin( cos( tg( ctg( arcsin( arccos( arctg( arcctg( ln( log( trunc( floor( ceil( abs( sqrt( exp("
+
+    val math = "sin cos tg ctg arcsin arccos arctg arcctg ln log trunc floor ceil abs sqrt exp"
+
+    val mathSequence = "^*/%+-&|!<<>>"
+
+    val hiPriority = "^!"
+    val miPriority = "*/%"
+    val lowPriority = "+-_&|<<>>"
 
     val postfixFunction = "!"
+
+    private fun getPriority(str: String): Int {
+        if (str in hiPriority) {
+            return 3
+        }
+        if (str in miPriority) {
+            return 2
+        }
+        if (str in lowPriority) {
+            return 1
+        }
+        return 0
+    }
 
     private fun parseToElementary(str: String): MutableList<String> {
         var fstr = str.filter { (it != ' ') }
@@ -34,7 +59,7 @@ class MathInterpreterImpl : MathInterpreter {
             } else if ((elementary[i] + elementary[i + 1]) in doublingsOperators) {
                 merged.add(elementary[i] + elementary[i + 1])
                 i += 1
-            } else if ((elementary[i] + elementary[i + 1]) in function) {
+            } else if ((elementary[i] + elementary[i + 1]) in functionWithBrakes) {
                 merged.add(elementary[i] + elementary[i + 1])
                 i += 1
             } else {
@@ -51,7 +76,7 @@ class MathInterpreterImpl : MathInterpreter {
             elementary[0] = "_"
         }
         for (i in 1 until elementary.size) {
-            if (elementary[i] == "-" && (elementary[i - 1] in function || elementary[i - 1] in elemStringParseSymbols)) {
+            if (elementary[i] == "-" && (elementary[i - 1] in functionWithBrakes || elementary[i - 1] in elemStringParseSymbols)) {
                 elementary[i] = "_"
             }
         }
@@ -65,7 +90,7 @@ class MathInterpreterImpl : MathInterpreter {
         return merged
     }
 
-    private fun toPolish(
+    private fun toPolishAndNum(
         mergedAndUnary: MutableList<String>,
         variables: Map<String, Double>
     ): MutableList<String> {
@@ -81,16 +106,69 @@ class MathInterpreterImpl : MathInterpreter {
                 polish.add(mergedAndUnary[i])
             } else if (mergedAndUnary[i] in variables.keys) {
                 polish.add(variables[mergedAndUnary[i]].toString())
-            } else if (mergedAndUnary[i] in function) {
+            } else if (mergedAndUnary[i] == "_") {
                 stack.add(mergedAndUnary[i])
-            } else if (mergedAndUnary[i] in function) {
+            } else if (mergedAndUnary[i] in "([") {
                 stack.add(mergedAndUnary[i])
+            } else if (mergedAndUnary[i] in functionWithBrakes) {
+                stack.add(mergedAndUnary[i])
+            } else if (mergedAndUnary[i] == ")") {
+                if (stack.isEmpty()) {
+                    throw IllegalArgumentException("(error with brackets")
+                }
+                while (stack.last() !in mathWithBrakes && stack.last() != "(") {
+                    polish.add(stack.last())
+                    stack.removeLast()
+                    if (stack.isEmpty()) {
+                        throw IllegalArgumentException("(error with brackets")
+                    }
+                }
+                if (stack.last() == "(") {
+
+                } else {
+                    polish.add(stack.last().substring(0, stack.last().length - 1))
+                }
+                stack.removeLast()
+            } else if (mergedAndUnary[i] == "]") {
+                if (stack.isEmpty()) {
+                    throw IllegalArgumentException("[error with brackets")
+                }
+                while (stack.last() != "[") {
+                    polish.add(stack.last())
+                    stack.removeLast()
+                    if (stack.isEmpty()) {
+                        throw IllegalArgumentException("[error with brackets")
+                    }
+                }
+
+                polish.add("trunc")
+                stack.removeLast()
+            } else if (mergedAndUnary[i] in mathSequence) {
+                if (stack.isEmpty()) {
+                    stack.add(mergedAndUnary[i])
+                    continue
+                }
+                var oPriority = getPriority(mergedAndUnary[i])
+                var sPriority = getPriority(stack.last())
+                while (stack.last() in math || sPriority >= oPriority || stack.last() == "_") {
+                    polish.add(stack.last())
+                    stack.removeLast()
+                    if (stack.isEmpty()) {
+                        break
+                    }
+                    sPriority = getPriority(stack.last())
+                }
+                stack.add(mergedAndUnary[i])
+            } else {
+                throw IllegalArgumentException("I dont know what is this")
             }
         }
+        while (stack.isNotEmpty()) {
+            polish.add(stack.last())
+            stack.removeLast()
+        }
 
-
-        return mergedAndUnary
-
+        return polish
     }
 
     override fun calcExpression(expression: String, variables: Map<String, Double>): Double {
@@ -106,6 +184,18 @@ class MathInterpreterImpl : MathInterpreter {
         println(findUnar(merge(parseToElementary("sin( sin(cos (ln(log(4)))) + 23.0 * 32.12)"))))
         println(findUnar(merge(parseToElementary("----23"))))
         println(findUnar(merge(parseToElementary("a + - 1 * -a + (-ds * - a + 3.2)"))))
+        println(
+            toPolishAndNum(
+                findUnar(merge(parseToElementary("3 + 4 * 2 / (1 - 5)^2"))),
+                mapOf<String, Double>()
+            )
+        )
+        println(
+            toPolishAndNum(
+                findUnar(merge(parseToElementary("exp(-1/2*3)"))),
+                mapOf<String, Double>()
+            )
+        )
 //        println(parseToElementary("123.23+213.2"))
 //        println(parseToElementary("123.23+213.2"))
         return 0.0
